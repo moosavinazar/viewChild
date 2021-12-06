0d75df4981680ea5bcb3d7f9628479f412996553
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-07 01:15:22 +0330

    encapsulation test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IncComponent } from './inc/inc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncComponent
   ],
   imports: [
     BrowserModule
