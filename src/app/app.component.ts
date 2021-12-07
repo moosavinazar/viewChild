c855d0f5ba6940891fbe6391396a6dd4d22fdfb6
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-07 17:03:03 +0330

    access component using ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, ViewEncapsulation} from '@angular/core';
+import {IncComponent} from "./inc/inc.component";
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,13 @@ export class AppComponent implements OnInit, AfterViewInit{
 
   // the meaning of ! is: https://docs.angular.lat/guide/template-expression-operators#the-non-null-assertion-operator---
   @ViewChild('par', {static: true}) par!: ElementRef;
+  @ViewChild(IncComponent, {static: true, read: IncComponent}) incComp!: IncComponent;
 
   constructor(private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
+    this.incComp.increment();
     this.par.nativeElement.innerHTML = "123456";
 
     // this.par.nativeElement.style.color = 'purple';
