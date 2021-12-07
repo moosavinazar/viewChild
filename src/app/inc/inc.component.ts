819b7d628aa8d67c09060e5ca7b93f85190f6dd9
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-07 17:09:01 +0330

    access public in component using ViewChild

--- src/app/inc/inc.component.ts
+++ src/app/inc/inc.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inc',
   templateUrl: './inc.component.html',
   styleUrls: ['./inc.component.css']
 })
-export class IncComponent implements OnInit {
+export class IncComponent {
 
   add: number = 0;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   public increment() {
     this.add++;
